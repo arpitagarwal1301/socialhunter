dfd2acc615517993dbf0e3c35cad530c6dc8c3ca
arpitagarwal1301 <arpitvinshu@gmail.com>
2019-11-26 01:23:01 +0530

    api files added,index.js modified, route issue exists

--- index.js
+++ index.js
@@ -2,7 +2,18 @@ const express = require("express");
 const connectDB = require("./config/db");
 
 const app = express();
+
+// Connect databse
 connectDB();
-const PORT = process.env.PORT || 5000;
+
 app.get('/',(req,res)=>res.send('API running') );
-app.listen(PORT,() =>console.log('Server running on port ${PORT}'));
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,() => console.log('Server running on port ${PORT}'));
